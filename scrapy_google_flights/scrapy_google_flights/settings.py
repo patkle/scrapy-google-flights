3bf5d63d0346f846006c5d89d4e9b0fe2e3445c3
__patrick__ <krustenkaese@yandex.com>
2020-11-18 21:18:20 +0100

    fixed folder structure

--- scrapy_google_flights/scrapy_google_flights/settings.py
+++
@@ -1,9 +0,0 @@
-# Scrapy settings for scrapy_google_flights project
-BOT_NAME = 'scrapy_google_flights'
-SPIDER_MODULES = ['scrapy_google_flights.spiders']
-NEWSPIDER_MODULE = 'scrapy_google_flights.spiders'
-
-ROBOTSTXT_OBEY = True
-
-SELENIUM_HEADLESS = False
-CHROME_DRIVER_PATH = None
