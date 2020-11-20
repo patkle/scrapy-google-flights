1cc36128a0b0ef71da88ae4a23b3b427203bd99b
__patrick__ <krustenkaese@yandex.com>
2020-11-20 18:46:19 +0100

    Added workaround for BadGzipFile exception

--- scrapy_google_flights/settings.py
+++ scrapy_google_flights/settings.py
@@ -13,7 +13,7 @@ logging.getLogger('pyppeteer').setLevel(logging.WARNING)
 PYPPETEER_HEADLESS = False
 
 # This is to avoid the add_reader() not implemented issue in asyncio.
-# This can occur if you are running Python 3.8 on Windows. 
+# This can occur if you are running Python 3.8 on Windows.
 # see: https://github.com/tornadoweb/tornado/issues/2751
 import sys
 import asyncio
@@ -24,3 +24,8 @@ if sys.platform == 'win32':
 # This is an experimental feature in Scrapy.
 # see: https://docs.scrapy.org/en/latest/topics/asyncio.html
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+
+# The following is a workaround to avoid dealing with BadGzipFile Exception
+# while converting pyppeteer response headers to scrapy response headers.
+# see: https://github.com/scrapy/scrapy/issues/2063
+COMPRESSION_ENABLED = False
