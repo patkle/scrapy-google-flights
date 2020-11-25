582745b18984c3e582ef2153e8ea6c5137aed1bc
Patrick Klein <kleinp@zohomail.eu>
2020-11-26 00:56:44 +0100

    Update http.py
    Added comments

--- scrapy_google_flights/http.py
+++ scrapy_google_flights/http.py
@@ -4,12 +4,19 @@ import pyppeteer
 
 
 class PyppeteerRequest(Request):
+    """Wrapper around Scrapy's Request to make requests using PyppeteerMiddleware"""
+    
     def __init__(self, *args, **kwargs) -> None:
+        # You can pass a function as callback which will be executed after the target
+        # page is loaded. The callback will receive the page object as parameter.
         self.pyppeteer_callback = kwargs.pop('pyppeteer_callback', None)
         super().__init__(*args, **kwargs)
 
 
 class PyppeteerResponse(HtmlResponse):
+    """Wrapper around Scrapy's HtmlResponse which creates a response from 
+    the objects provided by PyppeteerMiddleware"""
+    
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
